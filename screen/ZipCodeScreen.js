1022a91b4176acda2103036fa9bf6f40a30e2761
suttipong markchoochit <baysuttipong@gmail.com>
2022-08-27 22:44:13 +0700

    homescreen

--- screen/ZipCodeScreen.js
+++ screen/ZipCodeScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FlatList, View, Text, StyleSheet } from "react-native";
+import { FlatList, View, Text, StyleSheet, ImageBackground } from "react-native";
 import { TouchableHighlight } from "react-native-gesture-handler";
 import { useNavigation } from '@react-navigation/native'
 
@@ -9,6 +9,8 @@ const availableZipItems = [
     { place: 'Chiangmai', code: '50000' },
     { place: 'Khonkaen', code: '40000' },
     { place: 'Chonburi', code: '20000' },
+    { place: 'Bangkok', code: '10110'},
+    { place: 'Pattaya', code: '20150'},
    ]
    
    
@@ -26,11 +28,14 @@ const ZipItem = ({place, code, navigation}) => (
 export default function ZipCodeScreen(){
     const navigation = useNavigation()
     return (
+        <ImageBackground source={require('../bg2.jpg')} style={styles.backdrop}>
+            <Text style= {styles.top}>Choose Location</Text>
         <FlatList
         data = {availableZipItems}
         keyExtractor = {item => item.code}
         renderItem = {({item}) => <ZipItem {...item} navigation={navigation}/>}
     />    
+    </ImageBackground>
     )
 }
 
@@ -38,13 +43,25 @@ const styles = StyleSheet.create({
     zipItem: {
         flex: 1,
         flexDirection: 'row',
-        justifyContent: 'space-between'
+        justifyContent: 'space-between',  
     },
     zipPlace: {
         flex: 1,
     },
     zipCode: {
-
-
+        flex: 1
+    },
+    backdrop: {
+        flexDirection:'column',
+        justifyContent:'center',
+        alignItems:'center',
+        width: '100%',
+        height: '100%',
+    },
+    top: {
+        marginTop:30,
+        marginBottom:30,
+        fontSize:50,
+        textAlign:'center',
     }
 })
